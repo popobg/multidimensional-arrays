33cfd03a5bb1ac0545ffaab7d5d91243116e2cba
popobg <pauline.bouyssou1@gmail.com>
2024-06-09 09:44:30 +0200

    Add printing in the terminal for feedback

--- MultidimensionalArrays/MultidimensionalArrays/Program.cs
+++ MultidimensionalArrays/MultidimensionalArrays/Program.cs
@@ -5,8 +5,10 @@
         static void Main()
         {
             // transform bidimensional array into a list of list that can be modified
+            // print the result to the terminal
             static List<List<int>> ConvertArrayToList(int[,] array)
             {
+                Console.WriteLine("Tableau uniforme :");
                 var listOfList = new List<List<int>>();
 
                 for (int i = 0; i < array.GetLength(0); i++)
@@ -15,15 +17,19 @@
                     for (int j = 0; j < array.GetLength(1); j++)
                     {
                         listOfList[i].Add(array[i, j]);
+                        Console.Write(array[i, j]);
                     }
+                    Console.WriteLine();
                 }
+                Console.WriteLine();
 
                 return listOfList;
             }
 
+            // fill the jagged array with the list obtained from the regular array
             static int[][] fillArray(List<List<int>> originalList)
             {
-                var outputArray = new int[3][];
+                var outputArray = new int[originalList.Count()][];
                 var nestedListLength = originalList[0].Count();
 
                 for (int i = 0; i < nestedListLength; i++)
@@ -34,8 +40,24 @@
                 return outputArray;
             }
 
+            // display jagged array
+            static void DisplayArray(int[][] arr)
+            {
+                Console.WriteLine("Tableau irrégulier :");
+                
+                for (int i = 0; i < arr[0].Count(); i++)
+                {
+                    foreach (var item in arr[i])
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
             var bidimensionalArray = new int[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
             var jaggedArray = fillArray(ConvertArrayToList(bidimensionalArray));
+            DisplayArray(jaggedArray);
         }
     }
 }
